2431ac5b4bb9c10b05c5f5cb43b7ef4c81321fc3
kwangyoulsagong <sgky0511@naver.com>
2023-12-28 18:46:54 +0900

    [feat] extention 스크랩 가져오기

--- weblog-extension/src/App.js
+++ weblog-extension/src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 
 function App() {
   const [isContainerVisible, setIsContainerVisible] = useState(true);
-  const [scrapedContent, setScrapedContent] = useState("");
+  const [selectedHtml, setSelectedHtml] = useState("");
+  const [computedStyles, setComputedStyles] = useState({});
   const [isDragging, setIsDragging] = useState(false);
 
   const toggleContainerVisibility = () => {
@@ -33,21 +34,49 @@ function App() {
 
   const handleSelectionChange = () => {
     if (isDragging) return;
-    const selection = window.getSelection();
-    const selectedHTML = Array.from(
-      selection.rangeCount > -10
-        ? selection.getRangeAt(0).cloneContents().children
-        : []
-    )
-      .map((child) => child.outerHTML)
-      .join("");
 
-    setScrapedContent(`${selectedHTML}`);
-    console.log(selectedHTML);
+    const selection = window.getSelection();
+    const selectedRange = selection.getRangeAt(0);
+
+    // Create a temporary div to extract HTML content with styles
+    const tempDiv = document.createElement("div");
+    tempDiv.style.maxWidth = "100%";
+    tempDiv.appendChild(selectedRange.cloneContents());
+
+    // Get the computed styles for the selected content
+    const computedStyles = window.getComputedStyle(
+      selection.anchorNode.parentElement
+    );
+
+    // Extract the styles you need from the computedStyles object
+    const styles = {
+      color: computedStyles.color,
+      fontSize: computedStyles.fontSize,
+      fontWeight: computedStyles.fontWeight,
+      // Add other styles you are interested in
+    };
+
+    // Update computedStyles state
+    setComputedStyles(styles);
+
+    // Update selectedHtml state with the selected HTML content
+    setSelectedHtml(tempDiv.innerHTML);
   };
 
   const getStyledContent = () => {
-    return { __html: scrapedContent };
+    const inlineStyle = {
+      color: computedStyles.color,
+      fontSize: computedStyles.fontSize * 0.5,
+      fontWeight: computedStyles.fontWeight,
+      // Add other styles as needed
+    };
+
+    return (
+      <div
+        style={inlineStyle}
+        dangerouslySetInnerHTML={{ __html: selectedHtml }}
+      />
+    );
   };
 
   return (
@@ -55,10 +84,7 @@ function App() {
       {isContainerVisible && (
         <div className="extension-content">
           {/* <button onClick={toggleContainerVisibility}>보이기</button> */}
-          <div
-            className="scrab-box"
-            dangerouslySetInnerHTML={getStyledContent()}
-          />
+          <div className="scrab-box">{getStyledContent()}</div>
         </div>
       )}
     </div>
