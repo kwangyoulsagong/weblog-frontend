bea2c0083d4690c9c21bb682eb2ac5ee4d2451da
kwangyoulsagong <sgky0511@naver.com>
2023-12-29 17:49:30 +0900

    [feat] 메뉴 바 액션

--- weblog-extension/src/components/menu.js
+++ weblog-extension/src/components/menu.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
 const Menu = () => {
+  const letters = ["E", "V"]; // Add more letters if needed
+  const [selectedLetter, setSelectedLetter] = useState("E");
+
+  const toggleSelected = (letter) => {
+    setSelectedLetter((prevSelectedLetter) =>
+      prevSelectedLetter === letter ? null : letter
+    );
+  };
+
+  const getCircleStyle = (letter) => ({
+    backgroundColor: selectedLetter === letter ? "#AAB3FF" : "",
+    color: selectedLetter === letter ? "#ffffff" : "",
+    height: "100%",
+    width: "45%",
+    borderRadius: "50%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  });
+
   return (
     <div className="Menu-Container">
-      <span className="E">E</span> <span className="V">V</span>
+      {letters.map((letter) => (
+        <span
+          key={letter}
+          className={letter}
+          onClick={() => toggleSelected(letter)}
+          style={getCircleStyle(letter)}
+        >
+          {letter}
+        </span>
+      ))}
     </div>
   );
 };
+
 export default Menu;
