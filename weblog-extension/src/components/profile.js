3c691918c0c570a175a77755f0beb5ab43ef45ac
kwangyoulsagong <sgky0511@naver.com>
2023-12-29 15:46:54 +0900

    [feat] toggleButtonVisible-update

--- weblog-extension/src/components/profile.js
+++ weblog-extension/src/components/profile.js
@@ -1,12 +1,8 @@
 import React from "react";
 import profileImg from "../images/profile.png";
-import closeBtnImg from "../images/closeBtn.png";
 const profile = () => {
   return (
     <div className="porfile-container">
-      <div className="closeBtn">
-        <img src={closeBtnImg}></img>
-      </div>
       <circle className="profile-circle">
         <img src={profileImg} alt="profile" />
       </circle>
--- weblog-extension/src/extension.js
+++ weblog-extension/src/extension.js
@@ -1,90 +1,47 @@
-// App.js
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Profile from "./components/profile";
+import useDragDetect from "./hooks/useDragDetect";
+import useSelectionChange from "./hooks/useSelectionChange";
+import useStyledContent from "./module/useStyledContent";
+import closeBtnImg from "../src/images/closeBtn.png";
+import openBtnImg from "../src/images/profile.png";
 
 function App() {
   const [isContainerVisible, setIsContainerVisible] = useState(true);
   const [selectedHtml, setSelectedHtml] = useState("");
   const [computedStyles, setComputedStyles] = useState({});
   const [isDragging, setIsDragging] = useState(false);
+  const [btnClass, setBtnClass] = useState("closeBtn");
 
+  //보이기 숨기기 버튼
   const toggleContainerVisibility = () => {
     setIsContainerVisible((prev) => !prev);
-  };
-
-  useEffect(() => {
-    const handleDragStart = () => {
-      setIsDragging(true);
-    };
-
-    const handleDragEnd = () => {
-      setIsDragging(false);
-    };
-
-    document.addEventListener("selectionchange", handleSelectionChange);
-    document.addEventListener("dragstart", handleDragStart);
-    document.addEventListener("dragend", handleDragEnd);
-
-    return () => {
-      document.removeEventListener("selectionchange", handleSelectionChange);
-      document.removeEventListener("dragstart", handleDragStart);
-      document.removeEventListener("dragend", handleDragEnd);
-    };
-  }, []);
-
-  const handleSelectionChange = () => {
-    if (isDragging) return;
-    //드래그 할 위치
-    const selection = window.getSelection();
-    const selectedRange = selection.getRangeAt(0);
-
-    // temp div 생성 스크랩 할 내용의 스타일을 추출하는 div
-    const tempDiv = document.createElement("div");
-    tempDiv.style.maxWidth = "100%";
-    tempDiv.appendChild(selectedRange.cloneContents());
-
-    // 선택된 영역 스타일 가져옴
-    const computedStyles = window.getComputedStyle(
-      selection.anchorNode.parentElement
-    );
-
-    // 추출한 스타일들을 정의
-    const styles = {
-      color: computedStyles.color,
-      fontSize: computedStyles.fontSize,
-      fontWeight: computedStyles.fontWeight,
-      // Add other styles you are interested in
-    };
-
-    // 스타일 상태 업데이트
-    setComputedStyles(styles);
-
-    // tempDiv에 적용
-    setSelectedHtml(tempDiv.innerHTML);
-  };
-
-  const getStyledContent = () => {
-    const inlineStyle = {
-      color: computedStyles.color,
-      fontSize: computedStyles.fontSize * 0.5,
-      fontWeight: computedStyles.fontWeight,
-    };
-
-    return (
-      <div
-        style={inlineStyle}
-        dangerouslySetInnerHTML={{ __html: selectedHtml }}
-      />
+    setBtnClass((prevClass) =>
+      prevClass === "closeBtn" ? "openBtn" : "closeBtn"
     );
   };
+  const handleSelectionChange = useSelectionChange(
+    setSelectedHtml,
+    setComputedStyles,
+    isDragging
+  );
+
+  useDragDetect(handleSelectionChange, setIsDragging);
+
+  const getStyledContent = useStyledContent(computedStyles, selectedHtml);
 
   return (
     <div>
+      <div className={btnClass} onClick={toggleContainerVisibility}>
+        <img
+          src={btnClass === "closeBtn" ? closeBtnImg : openBtnImg}
+          alt="Button"
+        />
+      </div>
       {isContainerVisible && (
         <div className="extension-content">
           <Profile />
-          {/* <button onClick={toggleContainerVisibility}>보이기</button> */}
           <div className="scrab-box">{getStyledContent()}</div>
         </div>
       )}
