27c6fca5f61075c47f5ed023590255cf2bf678e6
kwangyoulsagong <sgky0511@naver.com>
2023-12-29 17:10:53 +0900

    [initial] menu 추가

---
+++ weblog-extension/src/components/menu.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Menu = () => {
+  return (
+    <div className="Menu-Container">
+      <span className="E">E</span> <span className="V">V</span>
+    </div>
+  );
+};
+export default Menu;
--- weblog-extension/src/extension.js
+++ weblog-extension/src/extension.js
@@ -7,19 +7,19 @@ import useStyledContent from "./module/useStyledContent";
 import closeBtnImg from "../src/images/closeBtn.png";
 import openBtnImg from "../src/images/profile.png";
 import Scrab from "./components/scrab";
-
+import Menu from "./components/menu";
 function App() {
-  const [isContainerVisible, setIsContainerVisible] = useState(true);
+  const [isContainerVisible, setIsContainerVisible] = useState(false);
   const [selectedHtml, setSelectedHtml] = useState("");
   const [computedStyles, setComputedStyles] = useState({});
   const [isDragging, setIsDragging] = useState(false);
-  const [btnClass, setBtnClass] = useState("closeBtn");
+  const [btnClass, setBtnClass] = useState("openBtn");
 
   //보이기 숨기기 버튼
   const toggleContainerVisibility = () => {
     setIsContainerVisible((prev) => !prev);
     setBtnClass((prevClass) =>
-      prevClass === "closeBtn" ? "openBtn" : "closeBtn"
+      prevClass === "openBtn" ? "closeBtn" : "openBtn"
     );
   };
   const handleSelectionChange = useSelectionChange(
@@ -36,12 +36,13 @@ function App() {
     <div>
       <div className={btnClass} onClick={toggleContainerVisibility}>
         <img
-          src={btnClass === "closeBtn" ? closeBtnImg : openBtnImg}
+          src={btnClass === "openBtn" ? openBtnImg : closeBtnImg}
           alt="Button"
         />
       </div>
       {isContainerVisible && (
         <div className="extension-content">
+          <Menu />
           <Profile />
           <Scrab getStyledContent={getStyledContent} />
         </div>
