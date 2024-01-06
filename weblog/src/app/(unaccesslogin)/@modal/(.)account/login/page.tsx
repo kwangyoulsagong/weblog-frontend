1c9922fb80cb487b7bf193e8e6085b974451538d
kwangyoulsagong <sgky0511@naver.com>
2024-01-06 20:07:37 +0900

    refactor: prviate_folder 추가 가독성

--- weblog/src/app/(unaccesslogin)/@modal/(.)account/login/page.tsx
+++ weblog/src/app/(unaccesslogin)/@modal/(.)account/login/page.tsx
@@ -1,41 +1,6 @@
-"use client"
-import { useState } from "react"
-import styles from "./login.module.css"
+import Loginmodal from "@/app/(unaccesslogin)/_component/LoginModal"
 export default function LoginModal(){
-    const [id, setId]=useState()
-    const [password, setPassword]=useState()
-    const [message, setMessage]=useState()
-
-    const onSubmit=()=>{}
-    const onCLose=()=>{}
-    const handleChangeId=()=>{}
-    const handleChangePassword=()=>{}
     return(
-        <div className={styles.modalBackground}>
-            <div className={styles.modal}>
-                <div className={styles.modalHeader}>
-                    <button className={styles.closeBtn} onClick={onCLose}>
-                        
-                    </button>
-                    <div>로그인하세요.</div>
-                </div>
-                <form onSubmit={onSubmit}>
-                <div className={styles.modalBody}>
-                    <div className={styles.inputDiv}>
-                        <label className={styles.inputLabel} htmlFor="id">아이디</label>
-                        <input id="id" className={styles.input} value={id} onChange={handleChangeId} type="text" placeholder=""/>
-                    </div>
-                    <div className={styles.inputDiv}>
-                        <label className={styles.inputLabel} htmlFor="password">비밀번호</label>
-                        <input id="password" className={styles.input} value={password} onChange={handleChangePassword} type="text" placeholder=""/>
-                    </div>
-                </div>
-                <div className={styles.message}>{message}</div>
-                <div className={styles.modalFooter}>
-                    <button className={styles.actionBtn} disabled={!id && !password}>로그인하기</button>
-                </div>
-            </form>
-            </div>
-        </div>
+       <Loginmodal />
     )
 }
