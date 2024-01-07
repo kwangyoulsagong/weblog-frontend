32a8db033ab8682f1e893b3ea8356b1b6d74166c
kwangyoulsagong <sgky0511@naver.com>
2024-01-07 19:35:43 +0900

    initial: 미리보기 격자 표현 기능 추가

--- weblog-extension/src/components/postpreview.js
+++ weblog-extension/src/components/postpreview.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./postpreview.css";
+import Collection2 from "../images/multiple.png";
+
 const PostPreview = () => {
+  const collection1Ref = useRef();
+  const collection2Ref = useRef();
+  const [collection, selectCollection] = useState("collect1column");
+
+  const handleCollection = (prev) => {
+    selectCollection((prevCollection) =>
+      prevCollection === "collect1column" ? "collect2coloumn" : "collect1column"
+    );
+  };
+  const getPreviewBoxStyle = () => {
+    if (collection === "collect2coloumn") {
+      return {
+        width: "125px",
+        height: "125px",
+        marginLeft: "15px",
+      };
+    }
+    return {};
+  };
+
   return (
     <div className="postContainer">
-      <div className="previewBox"></div>
-      <div className="previewBox"></div>
-      <div className="previewBox"></div>
-      <div className="previewBox"></div>
-      <div className="previewBox"></div>
-      <div className="previewBox"></div>
+      <div className="collectionMenu">
+        <div
+          className="collection1img"
+          ref={collection1Ref}
+          onClick={() => handleCollection(collection1Ref)}
+        ></div>
+        <img
+          className="collection2img"
+          ref={collection2Ref}
+          onClick={() => handleCollection(collection2Ref)}
+          src={Collection2}
+          alt="Collection2"
+        ></img>
+      </div>
+      <div className={collection}>
+        <div className="previewBox" style={getPreviewBoxStyle()}></div>
+        <div className="previewBox" style={getPreviewBoxStyle()}></div>
+        <div className="previewBox" style={getPreviewBoxStyle()}></div>
+        <div className="previewBox" style={getPreviewBoxStyle()}></div>
+        <div className="previewBox" style={getPreviewBoxStyle()}></div>
+        <div className="previewBox" style={getPreviewBoxStyle()}></div>
+      </div>
     </div>
   );
 };
+
 export default PostPreview;
