83cfccb6cd6a3dd92dc4c37b3ba14fc46f91684f
kwangyoulsagong <sgky0511@naver.com>
2024-01-10 18:15:33 +0900

    initial: 댓글기능 추가

--- weblog-extension/src/pages/mypost.js
+++ weblog-extension/src/pages/mypost.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import "./mypost.css";
+import Comment from "../components/comment";
 const MyPost = ({ contenti }) => {
   const [updateBtn, setUpdateBtn] = useState(false);
+  const onHandleSaveBtn = () => {
+    console.log("저장");
+  };
   const onHandleUpdateBtn = () => {
     setUpdateBtn(true);
+  };
+  const onHandleDeleteBtn = () => {
+    alert("현재 포스트가 삭제되었습니다.");
   };
   const Scrab = contenti;
   return (
@@ -20,10 +27,19 @@ const MyPost = ({ contenti }) => {
             hello
           </div>
         </div>
-        <button className="updateBtn" onClick={onHandleUpdateBtn}>
-          수정
-        </button>
-        <header className="myPostHeader"></header>
+
+        <header className="myPostHeader">
+          <button className="saveBtn" onClick={onHandleSaveBtn}>
+            저장
+          </button>
+          <button className="updateBtn" onClick={onHandleUpdateBtn}>
+            수정
+          </button>
+          <button className="deleteBtn" onClick={onHandleDeleteBtn}>
+            삭제
+          </button>
+          <Comment />
+        </header>
       </div>
     </div>
   );
