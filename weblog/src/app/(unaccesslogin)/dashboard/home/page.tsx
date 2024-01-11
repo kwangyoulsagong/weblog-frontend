661d0a72897ddc95374c01d32a5a44cd6b61fdde
kwangyoulsagong <sgky0511@naver.com>
2024-01-11 13:55:14 +0900

    feat: 홈 화면 구성

--- weblog/src/app/(unaccesslogin)/dashboard/home/page.tsx
+++ weblog/src/app/(unaccesslogin)/dashboard/home/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./home.module.css"
 export default function Home(){
     return(
-        <div className={styles.moduleBackground}>대시보드 홈</div>
+        <div className={styles.moduleBackground}>홈</div>
     )
 }
--- weblog/src/app/(unaccesslogin)/dashboard/layout.tsx
+++ weblog/src/app/(unaccesslogin)/dashboard/layout.tsx
@@ -1,15 +1,35 @@
 import { ReactNode } from "react";
-import styles from "./menubar.module.css"
+import styles from "./dashboard.module.css"
 import Image from "next/image";
 import Logo from "@/asset/images/logo.svg"
 type props = {children:ReactNode}
 export default function MenuBar({children}:props){
     return(
         <div className={styles.container}>
-            <div className={styles.moduleMenubar}>
-            <Image className={styles.logo} src={Logo} alt='logo' />
-            <line className={styles.moduleLine}></line>
-            </div>{children}</div>
+            <header className={styles.moduleLeftWrapper}>
+                <section className={styles.moduleLeftSection}>
+                  <div className={styles.moduleLeftFixedSection}></div>
+                </section>
+            {/* <Image className={styles.logo} src={Logo} alt='logo' />
+            <line className={styles.moduleLine}></line> */}
+            </header>
+            <div className={styles.moduleRightWrapper}>
+                <div className={styles.moduleRightSectionInner}>
+                  <main className={styles.moduleMain}>{children}</main>
+                  <section className={styles.moduleRightSection}>
+                    <form className={styles.moduleSearch}>
+                    <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                  <g>
+                    <path
+                      d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                  </g>
+                </svg>
+                <input type="search" />
+                    </form>
+                  </section>
+                </div>
+            </div>
+            </div>
     )
 
 }
