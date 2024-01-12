afd0ff855be69aedab1ff8a01e2ee07ecd597c58
kwangyoulsagong <sgky0511@naver.com>
2024-01-12 15:00:40 +0900

    initial: slide 기능 구현

--- weblog/src/app/(unaccesslogin)/dashboard/layout.tsx
+++ weblog/src/app/(unaccesslogin)/dashboard/layout.tsx
@@ -1,18 +1,46 @@
 "use client"
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import styles from "./dashboard.module.css"
 import Image from "next/image";
 import Logo from "@/asset/images/logo.svg"
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 type props = {children:ReactNode}
+const tabRedirectMap = {
+  popularTab: '/dashboard/home/popular',
+  recommendTab: '/dashboard/home/recommend',
+};
 export default function MenuBar({children}:props){
+  const tabRef=useRef<HTMLDivElement>(null)
   const [menubar , setMenubar]=useState(false)
   const [logoimg,setLogo]=useState(true)
   const onHandleMenubar = () => {
     setMenubar(!menubar);
     setLogo(!logoimg)
   };
-  
+  const router=useRouter()
+  const [tabMenu, setTabMenu]=useState<keyof typeof tabRedirectMap>("popularTab")
+  const onHandleTab=(prev:keyof typeof tabRedirectMap)=>{
+      setTabMenu(prev);
+      router.push(tabRedirectMap[prev])
+      localStorage.setItem('activeTab', prev);
+    
+  }
+  //현재 링크 유지
+  useEffect(() => {
+    const activeTab = localStorage.getItem('activeTab') as keyof typeof tabRedirectMap;
+
+    if (activeTab && tabRedirectMap[activeTab]) {
+      setTabMenu(activeTab);
+    }
+  }, []);
+  //메뉴바 이동 스크롤
+  useEffect(()=>{
+    if(tabRef.current){
+      tabRef.current.style.left=tabMenu==="recommendTab"? '100px':'0'
+      tabRef.current.style.transition="0.5s"
+    }
+  },[tabMenu])
     return(
         <div className={styles.container}>
             {menubar&&(
@@ -37,7 +65,14 @@ export default function MenuBar({children}:props){
                     </div>
                   </div>
                 <main className={styles.main}>
-                
+                <div className={styles.postTabContainer}>
+                    <div className={styles.postTab}>
+                      <div ref={tabRef} className={styles.tabActive}>
+                      </div>
+                      <section className={styles.popularTab}  onClick={()=> onHandleTab("popularTab")}> 인기포스트</section>
+                        <section className={styles.recommendTab } onClick={()=>onHandleTab("recommendTab")}> 추천순</section>
+                    </div>
+                </div>
                   {children}</main>
                 </div>
             </div>
