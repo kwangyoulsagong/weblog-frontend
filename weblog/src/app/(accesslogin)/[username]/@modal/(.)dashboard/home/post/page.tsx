2888c3947824dbf5b99ffa244b2fa219f70b4ded
kwangyoulsagong <sgky0511@naver.com>
2024-01-17 20:15:41 +0900

    design  previewBox layout 수정

---
+++ weblog/src/app/(accesslogin)/[username]/@modal/(.)dashboard/home/post/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import styles from "@/app/(accesslogin)/[username]/@modal/(.)dashboard/home/post/post.module.css"
+import { useRouter } from "next/navigation"
+import { useEffect, useRef } from "react"
+
+export default function Post(){
+    const postRef=useRef<HTMLDivElement>(null)
+    const router=useRouter()
+    const onHandleClose = ()=>{
+        if(postRef.current){
+            postRef.current.style.width="0px"
+            postRef.current.style.transition="0.4s"
+            const transitionEndHandler = () => {
+                router.back();
+            };
+
+            postRef.current.addEventListener('transitionend', transitionEndHandler);
+        }
+        
+    }
+    useEffect(()=>{
+        if(postRef.current){
+            postRef.current.style.width="900px"
+            postRef.current.style.transition="0.4s"
+        }
+    },[])
+
+    return(
+        <div className={styles.background} ref={postRef}>
+        <button onClick={onHandleClose}>닫기</button></div>
+    )
+}
