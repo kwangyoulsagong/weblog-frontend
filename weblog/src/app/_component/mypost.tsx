62843c3a0d9f6532eeb3d0e9dc3a4c97fc54657d
kwangyoulsagong <sgky0511@naver.com>
2024-02-22 16:16:47 +0900

    feat: 마이포스트 디자인

--- weblog/src/app/_component/mypost.tsx
+++ weblog/src/app/_component/mypost.tsx
@@ -16,10 +16,10 @@ interface Post {
     postId: number;
     nickname: string;
     title: string;
-    tags: { createdDate: string; modifiedDate: string; tagContent: string }[];
+    tags: string [];
     like_count: number;
     is_like: boolean;
-    image_url: string;
+    imageUrl: string;
     createdDate: string;
     modifiedDate: string;
   }
@@ -124,23 +124,27 @@ export default function MyPost(){
                 <div key={index} className={styles.wrapper} onClick={() => onHandlePost(value.postId)}>
                 <li className={styles.previewBox}>
                     <div className={styles.front}>
-                    <img  src={value.image_url} alt="image"></img>
+                    <img  src={value.imageUrl} alt="image"></img>
                     <h3>{value.title}</h3>
-                    <div><span>
-                    {value.modifiedDate
-                      ? `수정됨: ${formatRelativeTime(value.modifiedDate)}`
-                      : `작성됨: ${formatRelativeTime(value.createdDate)}`}
-                  </span></div>
                     <div className={styles.contentHeader}>
                     <div className={styles.profileCircle}>
                         <img src="https://velog.velcdn.com/images/hmmhmmhm/profile/352c0d2c-9f4d-4489-a5c5-64d789a66a4b/image.webp" alt=''></img>  
                     </div>
                     <div className={styles.likes}><Image src={likesIcon} alt="like"></Image></div>
-                    <div className={styles.postBy}> <span>post</span> {value.nickname}</div> 
-                    <span className={styles.likesCount}>{value.like_count}</span>
+                                <div className={styles.postBy}> <span>post</span> <b>{value.nickname}</b></div> 
+                                <span className={styles.likesCount}>{value.like_count}</span>
                     </div>
                     </div>
-                    <div className={styles.back}> <div className={styles.content}>
+                    <div className={styles.back}> 
+                    <img  src={value.imageUrl} alt="image"></img>
+                                 <div className={styles.content}>
+                                 <h3>{value.title}</h3>
+                                 <div className={styles.tags}>
+                                   {value.tags.map((value,index)=>(
+                                       <span key={index}>{value}</span>
+                                   ))}
+                                    
+                                 </div>
                     <p>신년카드 웹앱 이미지 새해인사 우체통 (Web) 서비스 링크: https://postbox.run 위 URL 에서 실제로 신년인사 카드를 작성해보실 수 있어요! 코육대 예시 항해 플러스 코육대에서 행사를 연다는 소식을 한참 뒤늦게 듣고 빠르게 도전해보기 위</p>
                     </div></div>  
                 </li>
