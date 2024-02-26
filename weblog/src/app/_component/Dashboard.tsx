8c2e8b257b8254f6323e257c72c6ca1a8dbb1c2c
kwangyoulsagong <sgky0511@naver.com>
2024-02-26 14:26:57 +0900

    refactor: 메인페이지 리디자인

--- weblog/src/app/_component/Dashboard.tsx
+++ weblog/src/app/_component/Dashboard.tsx
@@ -20,68 +20,19 @@ export default function Dashboard({children}:props){
   const leftWrapperRef = useRef<HTMLDivElement>(null!)
   const mainWrapperRef=useRef<HTMLDivElement>(null!)
   const [menubar, setMenubar] = useState(false);
-  const [logoimg, setLogo] = useState(true);
-  const [dropMenu,setDropMenu]=useState(false)
   const [showSearch, setShowSearch]=useState(false)
-  const onHandleLogout=()=>{
-    setIsLogin(false)
-    router.push("/dashboard/home")
-  }
-
-  const onHandleMenubar = () => {
-    setMenubar(!menubar);
-    if (leftWrapperRef.current&&mainWrapperRef.current) {
-      leftWrapperRef.current.style.width=!menubar? "110px":"30px";
-      mainWrapperRef.current.style.width=!menubar? "90vw":"95vw";
-      mainWrapperRef.current.style.transition="0.5s"
-
-    }
-  };
-  const handleDropdownMenu = ()=>{
-    setDropMenu((prev)=>!prev)
-  }
-  const handleSearchBar=()=>{
-    setShowSearch(!showSearch)
-  }
 
       return(
           <div className={styles.container}>
               {/* 여기안에는 데이터 접근 가능 */}
                 <header className={styles.leftWrapper} >
                 <section className={styles.leftSection}ref={leftWrapperRef} >
-                <button className={styles.menuBtn}  onClick={onHandleMenubar}>
-                  <Image src={menubar? cross: hamburger } alt="burger"></Image>
-                        </button>
                 {isLogin ? <AccessLinkBar /> : <UnAccessLinkBar />}
                 </section>
            
             </header>
               <div className={styles.mainWrapper} ref={mainWrapperRef} >
                   <div className={styles.mainSectionInner}>
-                  <div className={styles.meuBarSection}>
-                      <div className={styles.menuBarFixedSection}>
-                       
-                        {logoimg&& <Image className={styles.LogoImg} src={Logo} alt="Logo"></Image>}
-                        <div className={styles.profile_header}>
-                          <div className={styles.searchBar} onClick={handleSearchBar}>
-                            <span>검색</span>
-                            <div className={styles.iconSearch}></div>
-                          </div>
-                        
-                        {isLogin && <button className={styles.notification}></button>}
-                        {isLogin && <div className={styles.profileCircle} onClick={handleDropdownMenu}>
-                          <Image src={profile} alt="kwang"/>
-                        </div>}
-                       </div>
-                       {isLogin&& <div className={styles.dropdown}>
-                          <div className={styles.dropMenu}  style={{height:dropMenu? '100vh':'0px', transition: "0.5s"}}>
-                          {isLogin && <b className={styles.nickname}>{nickname}</b>}
-                             <Link href="">마이페이지</Link>
-                            <button onClick={onHandleLogout}>로그아웃</button>
-                          </div>
-                          </div>}
-                      </div>
-                    </div>
                   <main className={styles.main}>
                     {children}
                   </main>
--- weblog/src/app/_component/accesslogin/linkBar.tsx
+++ weblog/src/app/_component/accesslogin/linkBar.tsx
@@ -2,18 +2,21 @@
 import Link from "next/link";
 import styles from "@/app/_component/dashboard.module.css"
 import Image from "next/image";
-import dashboardImg from "@/asset/images/dashboard.svg"
-import userImg from "@/asset/images/user.svg"
-import mypostImg from "@/asset/images/post.svg"
-import treeImg from "@/asset/images/tree.svg"
+import dashboardImg from "@/asset/images/main/dashboard.png"
+import userImg from "@/asset/images/main/user.png"
+import mypostImg from "@/asset/images/main/mypost.png"
+import treeImg from "@/asset/images/main/tree.png"
+import faqImg from "@/asset/images/main/faq.png"
+import logoImg from "@/asset/images/logo.png"
 import { useContext, useState } from "react"
 import { AuthContext } from "../Provider/authProvider"
 import { useRouter } from "next/navigation"
 export default function LinkBar(){
     const {nickname}=useContext(AuthContext)
+    //닉네임 컨텍스트를 활용해 로그인여부확인 리다이렉트
     const router=useRouter();
     const [btnPressed,setBtnPressed]=useState<String>("home")
-
+    //링크 라우터 
     const handleHome=(prev:string)=>{
         router.push(`/${nickname}/dashboard/home`)
         setBtnPressed(prev)
@@ -26,21 +29,41 @@ export default function LinkBar(){
         router.push(`/${nickname}/dashboard/mypost`)
         setBtnPressed(prev)
     }
+    //네비게이션 바 
     return(
         <div className={styles.leftFixedSection}>
-        <div id="home" style={{ backgroundColor: btnPressed === "home" ? "rgba(0, 0, 0, 0.1" : "#f4f4f4" }} onClick={()=>handleHome("home")}>
-            <Image src={dashboardImg} alt="dashboardImg"></Image>
-            홈</div>
-        <div id="mypage" style={{ backgroundColor: btnPressed === "mypage" ? "rgba(0, 0, 0, 0.1" : "#f4f4f4" }} onClick={()=>handleMyProfile("mypage")}>
-            <Image src={userImg} alt="userImg"></Image>
-            마이페이지</div>
-        <div id="mypost" style={{ backgroundColor: btnPressed === "mypost" ? "rgba(0, 0, 0, 0.1" : "#f4f4f4" }} onClick={()=>handleMyPost("mypost")}>
-        <Image src={mypostImg}  alt="mypostImg"></Image>
-            내 포스트</div>
-        <div >
-            <Image src={treeImg} alt="treeImg"></Image>
-            지식 나무</div>
-        
-     </div>
-    )
+            <div className={styles.logoContainer}>
+                <div className={styles.ellipse}>
+                    <Image src={logoImg} alt="profile"></Image>
+                </div>
+                <b>Scrabler</b>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.menuHeader}>
+                <span>Menu</span>
+            </div>
+            <div className={styles.menuList}>
+               <div onClick={()=>handleHome("home")}>
+                    <Image src={dashboardImg} alt="dashboard"></Image>
+                    <span>대시보드</span>
+               </div>
+               <div onClick={()=>handleMyProfile("myprofile")}>
+                    <Image src={userImg} alt="userImg"></Image>
+                    <span>마이페이지</span>
+               </div>
+               <div onClick={()=>handleMyPost("mypost")}> 
+                    <Image src={mypostImg} alt="mypostImg"></Image>
+                    <span>마이포스트</span>
+               </div>
+               <div >
+                    <Image src={treeImg} alt="treeImg"></Image>
+                    <span>지식 나무</span>
+               </div>
+               <div>
+                    <Image src={faqImg} alt="userImg"></Image>
+                    <span>FAQ</span>
+               </div>
+            </div>
+        </div>
+    )   
 }
