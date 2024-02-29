dc8bfc10940d6193a9c323482e8b056ddd23f955
kwangyoulsagong <sgky0511@naver.com>
2024-02-29 22:35:43 +0900

    feat: 마이페이지 구현

--- weblog/src/app/_component/myprofile.tsx
+++ weblog/src/app/_component/myprofile.tsx
@@ -2,28 +2,58 @@
 import { useState } from "react"
 import styles from "./myprofile.module.css"
 import MyProfile from "./myprofile/myprofile"
+import alarmImg from "@/asset/images/main/notification.png"
+import messageImg from "@/asset/images/main/message.png"
+import Image from "next/image"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+type profileType={
+    nickname:string
+    imageUrl: string
+    email:string
+
+}
 
 export default function MyComponentProfile(){
-    const [datePostMenu, setDatePostMenu]=useState("프로필")
-    const handlePostTab= (tab:any)=>{
-        setDatePostMenu(tab)
+
+    async function onHandleProfile(){
+        try{
+            const response= await axios.get("http://localhost:8000/api/v1/profiles/mine")
+            return response.data;
+        }
+        catch(error){
+            console.error(error)
+        }
     }
-    const styleComponent = (tab:any) => {
-        return {
-            borderBottom: datePostMenu === tab ? "1px solid #7e7e80" : "none",
-        };
-      };
+
+    const {data:profileData, isLoading, isSuccess, isError}=useQuery<profileType>({
+        queryKey:["profileKey"],
+        queryFn:onHandleProfile,
+    })
+
     return(
         <div className={styles.container}>
-              <div className={styles.headerMenu}>
-                    <div style={styleComponent("프로필")} onClick={()=>handlePostTab("프로필")}>프로필</div>
-                    <div style={styleComponent("계정 공개 범위")} onClick={()=>handlePostTab("계정 공개 범위")}>계정 공개 범위</div>
-                    <div style={styleComponent("알림")} onClick={()=>handlePostTab("알림")}>알림</div>
-                    <div style={styleComponent("설정")} onClick={()=>handlePostTab("설정")}>설정</div>
-                    <div style={styleComponent("도움말")} onClick={()=>handlePostTab("도움말")}>도움말</div>
-             </div>
              <div className={styles.background}>
-                <MyProfile/>
+                <div className={styles.top}>
+                    <div className={styles.myProfileContainer}>
+                    <b>{profileData?.nickname}</b>
+                    <span>{profileData?.email}</span>
+                    </div>
+                    <div className={styles.alarmBox}>
+                    <Image src={alarmImg} alt="alarmImg"></Image>
+                    <span>알림</span>
+                </div>
+                <div className={styles.messageBox}>
+                    <Image src={messageImg} alt="messageImg"></Image>
+                    <span>메시지</span>
+                </div>
+                </div>
+                <div className={styles.bottom}>
+                    <div className={styles.privacyContainer}></div>
+                    <div className={styles.setting}></div>
+                    <div className={styles.plannerSetting}></div>
+                </div>
              </div>
         </div>
     )
